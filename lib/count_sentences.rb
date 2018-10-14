4542f78aa467a41982aab53b11fb4f5fb8551de9
Mendy <mendy.cohen@flatironschool.com>
2018-10-14 15:51:57 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    one = self.gsub("?", ".")
+    two = one.gsub("!", ".")
+    three = two.gsub(";", ".")
+    three.split(".").count
   end
 end
+
+binding.pry
