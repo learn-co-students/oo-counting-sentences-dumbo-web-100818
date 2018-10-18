fff0176917021ec64f68740be958656ed6696c12
github username <chao.alexander.123@gmail.com>
2018-10-17 20:01:40 -0400

    monkey patched the String class

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.length == 0
+      return 0
+    else
+      sentences = self.split(/[.?!]/)
+      mod = sentences.delete_if {|sen| sen == "" || sen == " "  }
+      return mod.count
+    end
   end
 end
+
+# string = "one. two. three?"
+# string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+# string.count_sentences
